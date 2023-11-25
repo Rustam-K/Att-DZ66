20529991d4a8dc78cf9e01d9a41a2a2f47860025
Rustam <autocentr@list.ru>
2023-11-25 11:14:11 +0500

    Аттестация DZ66

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int sum = CalcSum(m, n);
+
+Console.WriteLine($"Сумма натуральных чисел от {m} до {n}: {sum}");
+
+
+int CalcSum(int start, int end)
+{
+    if (start > end)
+    {
+        return 0;
+    }
+    return start + CalcSum(start + 1, end);
+}
